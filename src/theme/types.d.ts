8aef1cdd3c6682d43a8b98cb8b6ef319c88f14ec
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-06-23 11:47:36 -0300

    md se agregan los types del theme

--- src/theme/types.d.ts
+++ src/theme/types.d.ts
@@ -1,7 +1,5 @@
 // src/theme/types.d.ts
 
-import '@mui/material/styles';
-
 declare module '@mui/material/styles' {
   interface PaletteColor {
     50?: string;
@@ -30,21 +28,166 @@ declare module '@mui/material/styles' {
     900?: string;
     950?: string;
   }
-}
 
-// Opcional: si usas Theme.palette.primary.50 directamente sin corchetes
-declare module '@mui/material' {
-  interface TypeBackground {
-    50?: string;
-    100?: string;
-    200?: string;
-    300?: string;
-    400?: string;
-    500?: string;
-    600?: string;
-    700?: string;
-    800?: string;
-    900?: string;
-    950?: string;
+  interface Palette {
+    grey: {
+      50: string;
+      100: string;
+      200: string;
+      300: string;
+      400: string;
+      500: string;
+      600: string;
+      700: string;
+      800: string;
+      900: string;
+      950: string;
+    };
+    primary: {
+      50: string;
+      100: string;
+      200: string;
+      300: string;
+      400: string;
+      500: string;
+      600: string;
+      700: string;
+      800: string;
+      900: string;
+      950: string;
+    };
+    secondary: {
+      50: string;
+      100: string;
+      200: string;
+      300: string;
+      400: string;
+      500: string;
+      600: string;
+      700: string;
+      800: string;
+      900: string;
+      950: string;
+    };
+    info: {
+      50: string;
+      100: string;
+      200: string;
+      300: string;
+      400: string;
+      500: string;
+      600: string;
+      700: string;
+      800: string;
+      900: string;
+      950: string;
+    };
+    success: {
+      50: string;
+      100: string;
+      200: string;
+      300: string;
+      400: string;
+      500: string;
+      600: string;
+      700: string;
+      800: string;
+      900: string;
+      950: string;
+    };
+    error: {
+      50: string;
+      100: string;
+      200: string;
+      300: string;
+      400: string;
+      500: string;
+      600: string;
+      700: string;
+      800: string;
+      900: string;
+      950: string;
+    };
+  }
+
+  interface PaletteOptions {
+    grey?: {
+      50?: string;
+      100?: string;
+      200?: string;
+      300?: string;
+      400?: string;
+      500?: string;
+      600?: string;
+      700?: string;
+      800?: string;
+      900?: string;
+      950?: string;
+    };
+    primary?: {
+      50?: string;
+      100?: string;
+      200?: string;
+      300?: string;
+      400?: string;
+      500?: string;
+      600?: string;
+      700?: string;
+      800?: string;
+      900?: string;
+      950?: string;
+    };
+    secondary?: {
+      50?: string;
+      100?: string;
+      200?: string;
+      300?: string;
+      400?: string;
+      500?: string;
+      600?: string;
+      700?: string;
+      800?: string;
+      900?: string;
+      950?: string;
+    };
+    info?: {
+      50?: string;
+      100?: string;
+      200?: string;
+      300?: string;
+      400?: string;
+      500?: string;
+      600?: string;
+      700?: string;
+      800?: string;
+      900?: string;
+      950?: string;
+    };
+    success?: {
+      50?: string;
+      100?: string;
+      200?: string;
+      300?: string;
+      400?: string;
+      500?: string;
+      600?: string;
+      700?: string;
+      800?: string;
+      900?: string;
+      950?: string;
+    };
+    error?: {
+      50?: string;
+      100?: string;
+      200?: string;
+      300?: string;
+      400?: string;
+      500?: string;
+      600?: string;
+      700?: string;
+      800?: string;
+      900?: string;
+      950?: string;
+    };
   }
 }
