e7987c695f18967595253696cce933fbdfa605c3
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-06-25 15:34:59 -0300

    md HomePage - se importa el DinoGame

--- src/modules/home/page/HomePage.tsx
+++ src/modules/home/page/HomePage.tsx
@@ -1,13 +1,14 @@
 // src/modules/home/page/HomePage.tsx
 import React from "react";
 import { Hero } from "../components/Hero";
+import { DinoGame } from "../components/DinoGame";
 
 export const HomePage: React.FC = () => {
  
   return (
     <>
       <Hero />
-
+      <DinoGame />
     </>
   );
 };
