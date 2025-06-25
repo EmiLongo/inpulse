89d42bf0600aa757814ac59e059e97a32b65646d
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2025-06-25 18:53:36 -0300

    md textStyles - se agregan estilos de titulos

--- src/theme/textStyles.tsx
+++ src/theme/textStyles.tsx
@@ -191,3 +191,44 @@ export const InputError = styled((props: TypographyProps) => <Typography {...pro
   ...inputAdvice(),
   color: theme.palette.error.main,
 }));
+
+
+
+
+import React from "react";
+
+interface Props {
+  children: React.ReactNode
+  sx?: object
+  id?: string
+}
+export const SectionTitle: React.FC<Props> = ({ children, sx, id }) => {
+  return (
+    <Title1
+      id={id}
+      sx={{
+        marginTop: {xs:"3rem", lg:"3rem"},
+        marginBottom: "1rem",
+        textAlign: "center",
+      ...sx }}>
+      { children }
+    </Title1>
+  )
+};
+
+export const SectionSubTitle: React.FC<Props> = ({ children, sx, id }) => {
+  return (
+    <Text1
+    id={id}
+    sx={{
+      textAlign: "center", 
+      marginBottom: "2rem",
+      paddingX: {xs: "2rem", md: "unset"}, 
+      maxWidth: {xs: "unset", md: "600px"}, 
+      textWrap: "balance",
+      ...sx}}
+    >
+      { children }
+    </Text1>
+  )
+}
