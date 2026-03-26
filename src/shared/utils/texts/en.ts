31d3002cfe7398d0c7664a18fb866fc98598e391
Emiliano Longo <140227770+EmiLongo@users.noreply.github.com>
2026-03-25 21:09:51 -0300

    md utils.texts, se agregan los textos en espaniol

--- src/shared/utils/texts/en.ts
+++ src/shared/utils/texts/en.ts
@@ -23,7 +23,23 @@ const en = {
     subtitle2b: "We take your essence as inspiration.",
     subtitle2c: "We create designs that convey trust.",    
     button: "Write to us",
-  }
+  },
+  whatWeDo: {
+    title1: "MATERIALIZAMOS TU...",
+    subtitle1: "Somos un estudio de diseño digital.",
+    product1Title: "Tienda Web",
+    product1Subtitle: "Mostrá tus productos online y agilizá tus ventas.",
+    product2Title: "Portfolio Virtual",
+    product2Subtitle: "Presentá tus trabajos de manera profesional y atractiva.",
+    product3Title: "Landing Page",
+    product3Subtitle: "Conquistá y convertí tus visitantes en clientes.",
+    product4Title: "Web Empresarial",
+    product4Subtitle: "Dale presencia digital a tu negocio y mostrá su esencia.",
+    title2a: "Identidad de Marca",
+    title2b: "Diseño",
+    title2c: "Desarrollo web",
+    subtitle2: "realizados con Inteligencia Artesanal y calidez humana.",
+  },
 };
 
 export default en;
